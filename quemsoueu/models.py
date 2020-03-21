d7322e60b6ad05a2ceaf09c70a20c60a96ed8784
piero <piero.desenzi@gmail.com>
2020-03-21 20:18:05 -0300

    home + wait + db

--- quemsoueu/models.py
+++ quemsoueu/models.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-from app import db
+from quemsoueu import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
 
-def write_to_db(object):
-    db.session.add(object)
-    db.session.commit()
+class StartFlag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ready = db.Column(db.Boolean, nullable=False)
--- quemsoueu/routes.py
+++ quemsoueu/routes.py
@@ -1,10 +1,26 @@
-from flask import render_template, url_for, flash, redirect
-from quemsoueu import app
+from flask import render_template, url_for, flash, redirect, request, make_response
+from quemsoueu import app, db
 from quemsoueu.forms import RegistrationForm
+from quemsoueu.models import User, StartFlag
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
     form = RegistrationForm()
-
+    if form.validate_on_submit():
+        user = User(username=form.username.data)
+        db.session.add(user)
+        db.session.commit()
+        #flash('Your account has been created! You are now able to log in', 'success')
+        res = make_response(redirect(url_for('wait')))
+        res.set_cookie('myname', form.username.data, max_age=60*60*24)
+        print(form.username.data)
+        print(request.cookies.get('myname'))
+        return res
     return render_template('home.html', form=form)
+
+
+@app.route("/wait")
+def wait():
+    return render_template('wait.html', name=request.cookies.get('myname'))
+    
