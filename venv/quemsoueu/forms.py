df528fbd9352808e69aadeddde3c332c786ffd2d
piero <piero.desenzi@gmail.com>
2020-03-21 14:23:47 -0300

    preadequation

--- venv/quemsoueu/forms.py
+++ venv/quemsoueu/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
